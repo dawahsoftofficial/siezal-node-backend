3b2b023b0207aeb002724130248575f78981df06
Sajawal Fareedi <sajawalfareedi448@gmail.com>
2025-09-09 22:42:29 +0500

    Feat: Updated Products Seeder

--- src/database/seeders/real-product.seeder.ts
+++ src/database/seeders/real-product.seeder.ts
@@ -11,10 +11,18 @@ import { EInventoryStatus } from "src/common/enums/inventory-status.enum";
 import { S3Service } from "src/shared/aws/s3.service";
 import { ConfigService } from "@nestjs/config";
 
+interface ImageFolder {
+  name: string;
+  path: string;
+  parent?: string;
+  images: string[];
+  subFolders: ImageFolder[];
+}
+
 export default class RealProductSeeder {
   private static usedSlugs = new Set<string>();
 
-  constructor() {}
+  constructor() { }
 
   private static makeUniqueSlug(title: string): string {
     let slug = title
@@ -32,6 +40,72 @@ export default class RealProductSeeder {
     return uniqueSlug;
   }
 
+  // private static readImageDir(dirPath: string, parent?: string): ImageFolder {
+  //   const folder: ImageFolder = {
+  //     name: path.basename(dirPath),
+  //     path: dirPath,
+  //     parent,
+  //     images: [],
+  //     subFolders: [],
+  //   };
+
+  //   const entries = fs.readdirSync(dirPath, { withFileTypes: true });
+
+  //   for (const entry of entries) {
+  //     const fullPath = path.join(dirPath, entry.name);
+
+  //     if (entry.isDirectory()) {
+  //       // Recurse into subfolder
+  //       const subFolder = RealProductSeeder.readImageDir(fullPath, folder.name);
+  //       folder.subFolders.push(subFolder);
+  //     } else {
+  //       // Treat file as image
+  //       const ext = path.extname(entry.name).toLowerCase();
+  //       if ([".jpg", ".jpeg", ".png", ".gif", ".webp"].includes(ext)) {
+  //         folder.images.push(fullPath);
+  //       }
+  //     }
+  //   }
+
+  //   return folder;
+  // }
+
+  private static slugify(input: string): string {
+    return input
+      .toLowerCase()
+      // replace everything except letters, numbers, |, and - with "-"
+      .replace(/[^a-z0-9|]+/g, "-")
+      // collapse multiple "-" into one
+      .replace(/-+/g, "-")
+      // trim leading/trailing "-"
+      .replace(/^-+|-+$/g, "");
+  }
+
+  private static buildMaps(
+    folder: ImageFolder,
+    imageMap: Map<string, string>,
+    imageParentMap: Map<string, string>,
+    parentPath: string[] = []
+  ) {
+    const currentPath = [...parentPath, folder.name];
+
+    // Add images in current folder
+    for (const imgPath of folder.images) {
+      const name = path.parse(imgPath).name;
+
+      imageMap.set(name, imgPath);
+
+      // Parent chain (excluding the image file itself)
+      imageParentMap.set(name, RealProductSeeder.slugify(currentPath.join("|")).replace("images|", ""));
+    }
+
+    // Recurse into subfolders
+    for (const sub of folder.subFolders) {
+      RealProductSeeder.buildMaps(sub, imageMap, imageParentMap, currentPath);
+    }
+  }
+
+
   public static async run(dataSource: DataSource): Promise<void> {
     const configService = new ConfigService();
     const s3Service = new S3Service(configService);
@@ -40,7 +114,6 @@ export default class RealProductSeeder {
     const productRepo = dataSource.getRepository(Product);
 
     const excelPath = path.join(__dirname, "../../data/products.xlsx");
-    const imageDir = path.join(__dirname, "../../data/images");
 
     const workbook = XLSX.readFile(excelPath);
     const sheet = workbook.Sheets[workbook.SheetNames[0]];
@@ -50,14 +123,18 @@ export default class RealProductSeeder {
     const parentMap = new Map<string, number>();
 
     await dataSource.transaction(async (manager) => {
-      const imageFiles = fs.readdirSync(imageDir);
+      // const structure = RealProductSeeder.readImageDir(imageDir);
+
+      // const jsonPath = path.join(__dirname, "image-structure.json");
+      // fs.writeFileSync(jsonPath, JSON.stringify(structure, null, 2), "utf-8");
+
+      const jsonPath = path.join(__dirname, "../../data/image-structure.json");
+      const structure = JSON.parse(fs.readFileSync(jsonPath, "utf-8"));
 
       const imageMap = new Map<string, string>();
+      const titleToCategoryMap = new Map<string, string>();
 
-      imageFiles.forEach((file) => {
-        const name = path.parse(file).name.toLowerCase();
-        imageMap.set(name, path.join(imageDir, file));
-      });
+      RealProductSeeder.buildMaps(structure, imageMap, titleToCategoryMap);
 
       const products: Product[] = [];
 
@@ -66,14 +143,25 @@ export default class RealProductSeeder {
 
       for (const row of rows) {
         const title: string = row["Title"];
-        const categorySlug: string = row["  Category-Slug"];
+        let categorySlug = titleToCategoryMap.get(title)
 
-        const [parentNameRaw, childNameRaw] = categorySlug.split(" - ");
-        const parentName = parentNameRaw.trim();
-        const childName = childNameRaw.trim();
-        const catKey = `${parentName} - ${childName}`;
+        if (!categorySlug) {
+          categorySlug = 'misc|other'
+        }
+
+        const [parentNameRaw, childNameRaw] = categorySlug.split("|");
+        const parentName = parentNameRaw.trim().replace(/-/g, " ").split(" ").map(word => {
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        })
+          .join(" ");
+        const childName = childNameRaw ? childNameRaw.trim().replace(/-/g, " ").split(" ").map(word => {
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        })
+          .join(" ") : undefined;
+        const catKey = childName ? `${parentName} - ${childName}` : parentName;
 
         let categoryId = categoryMap.get(catKey);
+
         if (!categoryId) {
           let parentId = parentMap.get(parentName);
 
@@ -81,8 +169,8 @@ export default class RealProductSeeder {
             const parent = categoryRepo.create({
               name: parentName,
               slug: parentName.toLowerCase().replace(/\s+/g, "-"),
-              icon: "placeholder-icon.png",
-              images: ["placeholder-image.png"],
+              icon: "placeholder.svg",
+              images: ["placeholder.svg"],
               slideShow: false,
               isFeatured: true,
               position: parentPosition++,
@@ -94,66 +182,73 @@ export default class RealProductSeeder {
             parentMap.set(parentName, parentId!);
           }
 
-          const child = categoryRepo.create({
-            name: childName,
-            slug: childName.toLowerCase().replace(/\s+/g, "-"),
-            icon: "placeholder-icon.png",
-            images: ["placeholder-image.png"],
-            slideShow: false,
-            isFeatured: false,
-            position: childPosition++,
-            parentId,
-          });
+          if (childName) {
+            const child = categoryRepo.create({
+              name: childName,
+              slug: childName.toLowerCase().replace(/\s+/g, "-"),
+              icon: "placeholder.svg",
+              images: ["placeholder.svg"],
+              slideShow: false,
+              isFeatured: false,
+              position: childPosition++,
+              parentId,
+            });
 
-          const savedChild = await manager.save(child);
+            const savedChild = await manager.save(child);
 
-          categoryId = savedChild.id;
-          categoryMap.set(catKey, categoryId!);
+            categoryId = savedChild.id;
+            categoryMap.set(catKey, categoryId!);
+          } else {
+            categoryId = parentId;
+            categoryMap.set(catKey, categoryId!);
+          }
         }
+
+        let imageUrl = "placeholder.svg"
 
         const imageFilePath = imageMap.get(title.toLowerCase());
 
-        if (!imageFilePath) {
-          console.warn(`Image not found for product: ${title}`);
-          continue;
+        if (imageFilePath) {
+          const buffer = fs.readFileSync(imageFilePath);
+
+          const fakeFile: Express.Multer.File = {
+            fieldname: "file",
+            originalname: path.basename(imageFilePath),
+            encoding: "7bit",
+            mimetype: mime.lookup(imageFilePath) || "image/jpeg",
+            size: buffer.length,
+            buffer,
+            stream: fs.createReadStream(imageFilePath),
+            destination: "",
+            filename: path.basename(imageFilePath),
+            path: imageFilePath,
+          };
+
+          const { url } = await s3Service.uploadImage(fakeFile);
+
+          imageUrl = url;
         }
-
-        const buffer = fs.readFileSync(imageFilePath);
-
-        const fakeFile: Express.Multer.File = {
-          fieldname: "file",
-          originalname: path.basename(imageFilePath),
-          encoding: "7bit",
-          mimetype: mime.lookup(imageFilePath) || "image/jpeg",
-          size: buffer.length,
-          buffer,
-          stream: fs.createReadStream(imageFilePath),
-          destination: "",
-          filename: path.basename(imageFilePath),
-          path: imageFilePath,
-        };
-
-        const { url } = await s3Service.uploadImage(fakeFile);
 
         const product = productRepo.create({
           sku: row["SKU"],
           title,
           slug: this.makeUniqueSlug(title),
-          shortDescription: row["  Short Description"],
-          description: row["  Description"],
-          seoTitle: row["  SEO Title"],
-          seoDescription: row["  SEO Description"],
-          price: parseFloat(row["  Price"]),
-          salePrice: row["  Sale Price"]
-            ? parseFloat(row["  Sale Price"])
+          shortDescription: row["Short Description"],
+          description: row["Description"],
+          seoTitle: row["SEO Title"],
+          seoDescription: row["SEO Description"],
+          price: parseFloat(row["Price"] || 100) || 100,
+          salePrice: row["Sale Price"] || 0
+            ? parseFloat(row["Sale Price"])
             : undefined,
-          stockQuantity: parseInt(row["  Stock Quantity"], 10) || 0,
-          status: EInventoryStatus.AVAILABLE,
+          stockQuantity: 100,
+          status: row['Status'] ? row['Status'] as EInventoryStatus : EInventoryStatus.OUT_OF_STOCK,
           categoryId,
           inventoryId: 1,
-          image: url,
+          image: imageUrl,
           unit: EProductUnit.PIECE,
           isGstEnabled: true,
+          gstFee: 18
         });
 
         products.push(product);
